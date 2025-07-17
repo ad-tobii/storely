7eed3e0b273126dfaf7899b3d2c0f10243a01bc8
ad-tobii <165202429+ad-tobii@users.noreply.github.com>
2025-07-17 05:43:01 +0100

    full deployments

--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -16,8 +16,7 @@ export const generateToken = (userId, role, res) => {
   res.cookie('jwt', token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: 'strict',
-    // This is the key change: the cookie will only be sent over HTTPS in production
+    sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
     secure: process.env.NODE_ENV === 'production',
   });
   return token;
